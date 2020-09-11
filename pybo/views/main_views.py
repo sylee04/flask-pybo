3ef89b5ce70e3dee12bdc4941bd7f54452aacfb3
sylee <remailme@hanmail.net>
2020-09-11 17:19:35 +0900

    플라스크 파이보 프로젝트 최초 커밋

---
+++ pybo/views/main_views.py
@@ -0,0 +1,39 @@
+# from flask import Blueprint, render_template
+# from pybo.models import Question
+#
+# bp = Blueprint('main', __name__, url_prefix='/')
+#
+# # @bp.route('/')
+# # def hello_pybo():
+# #     return 'Hello, Pybo!!'
+#
+# @bp.route('/hello')
+# def hello_pybo():
+#     return 'Hello, Pybo!!'
+#
+# @bp.route('/')
+# def index():
+#     # return 'Pybo index'
+#     question_list = Question.query.order_by(Question.create_date.desc())
+#     return render_template('question/question_list.html', question_list=question_list)
+#
+# @bp.route('/detail/<int:question_id>/')
+# def detail(question_id):
+#     # question = Question.query.get(question_id)
+#     question = Question.query.get_or_404(question_id)
+#     return render_template('question/question_detail.html', question=question)
+
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
+
+bp = Blueprint('main', __name__, url_prefix='/')
+
+
+@bp.route('/hello')
+def hello_pybo():
+    return 'Hello, Pybo!'
+
+
+@bp.route('/')
+def index():
+    return redirect(url_for('question._list'))
