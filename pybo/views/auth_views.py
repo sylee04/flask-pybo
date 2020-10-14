de44f99aed035b2902cfdf568bc831667304b1c8
sylee <remailme@hanmail.net>
2020-10-14 18:04:16 +0900

    1014;~3-09글쓴이표시

--- pybo/views/auth_views.py
+++ pybo/views/auth_views.py
@@ -6,6 +6,9 @@ from pybo import db
 from pybo.forms import UserCreateForm, UserLoginForm
 from pybo.models import User
 
+import functools
+
+
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
@@ -57,3 +60,10 @@ def logout():
     return redirect(url_for('main.index'))
 
 
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view
